be7248b5f86dbec18ff2042a354850f579a4a686
RubenYildiz21 <yildiz-ruben@hotmail.com>
2024-10-13 13:12:49 +0200

    Ad project to git

--- Stas.Monitor.Domains/obj/Debug/net6.0/Stas.Monitor.Domains.AssemblyInfo.cs
+++ Stas.Monitor.Domains/obj/Debug/net6.0/Stas.Monitor.Domains.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stas.Monitor.Domains")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe20f6d9481b3ce324418b86691f42a4490cb532")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stas.Monitor.Domains")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stas.Monitor.Domains")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
